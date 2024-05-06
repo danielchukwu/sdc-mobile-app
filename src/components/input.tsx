fcc48b51e8f6a5983558866cdbc84a979895a887
dc <00danzy@gmail.com>
2024-05-06 22:41:05 +0100

    feat: add IconInput, Input, and FormInput

---
+++ src/components/input.tsx
@@ -0,0 +1,42 @@
+import { useController, type Control, type FieldValues } from "react-hook-form";
+import { TextInput, TextInputProps, View } from "react-native";
+
+export const IconInput = ({
+  icon,
+  ...props
+}: TextInputProps & { icon: React.ReactNode }) => {
+  return (
+    <View className="relative">
+      {icon}
+      <Input {...props} />
+    </View>
+  );
+};
+
+export const Input = ({ className, ...props }: TextInputProps) => {
+  return (
+    <TextInput
+      {...props}
+      className="p-3 pl-14 pr-5 rounded-lg border border-gray-200 text-lg"
+    />
+  );
+};
+
+type FormInputProps = TextInputProps & {name: string, control: Control<FieldValues, any>};
+export const FormInput = ({ className, name, control, ...props }: FormInputProps) => {
+  const { field } = useController({
+    control,
+    defaultValue: '',
+    name
+  });
+
+  return (
+    <TextInput
+      {...props}
+      value={field.value}
+      onChangeText={field.onChange}
+      className="py-3 px-5 rounded-lg border border-gray-200 text-lg"
+    />
+  );
+};
+

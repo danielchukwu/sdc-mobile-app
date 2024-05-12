8f02e92c9e7f7fa143a079a073d0d3e6cd85c273
dc <00danzy@gmail.com>
2024-05-12 08:49:19 +0100

    feat: add sitemap link

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -4,8 +4,8 @@ export default function Layout() {
   return (
     <Stack screenOptions={{statusBarColor: "#fff", headerTitleAlign: 'center', headerShadowVisible: false}}>
       <Stack.Screen name="index" options={{ headerTitle: (data) => (<Text className="text-cc-primary-main text-xl font-semibold">SDC APP</Text>), headerRight: (props) => (
-        // <Link href={'/_sitemap'}>SM</Link>
-        <></>
+        <Link href={'/_sitemap'}>SM</Link>
+        // <></>
       ) }} />
       <Stack.Screen name="cases" options={{ headerShown: false }} />
     </Stack>

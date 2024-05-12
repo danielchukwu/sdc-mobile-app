d2d5e5acfd82851331f669c5cfafb5cc64da4f4b
dc <00danzy@gmail.com>
2024-05-12 08:50:43 +0100

    log hello world

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Link, Stack } from "expo-router";
 import { Text} from 'react-native';
 export default function Layout() {
+  console.log('Hello world');
   return (
     <Stack screenOptions={{statusBarColor: "#fff", headerTitleAlign: 'center', headerShadowVisible: false}}>
       <Stack.Screen name="index" options={{ headerTitle: (data) => (<Text className="text-cc-primary-main text-xl font-semibold">SDC APP</Text>), headerRight: (props) => (

a12598511ce31895920906390d2723404d785ccb
dc <00danzy@gmail.com>
2024-05-12 10:07:05 +0100

    clean up

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Link, Stack } from "expo-router";
 import { Alert, Text} from 'react-native';
 export default function Layout() {
-  Alert.alert('Send money...')
   return (
     <Stack screenOptions={{statusBarColor: "#fff", headerTitleAlign: 'center', headerShadowVisible: false}}>
       <Stack.Screen name="index" options={{ headerTitle: (data) => (<Text className="text-cc-primary-main text-xl font-semibold">SDC APP</Text>), headerRight: (props) => (

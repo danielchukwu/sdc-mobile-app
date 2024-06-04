f900f16d184c68967ff756b2a25724fb6a0b8cc8
dc <00danzy@gmail.com>
2024-06-04 08:52:49 +0100

    refactor

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -5,7 +5,6 @@ export default function Layout() {
     <Stack screenOptions={{statusBarColor: "#fff", headerTitleAlign: 'center', headerShadowVisible: false}}>
       <Stack.Screen name="index" options={{ headerTitle: (data) => (<Text className="text-cc-primary-main text-xl font-semibold">SDC APP</Text>), headerRight: (props) => (
         <Link href={'/_sitemap'}>SM</Link>
-        // <></>
       ) }} />
       <Stack.Screen name="cases" options={{ headerShown: false }} />
     </Stack>
